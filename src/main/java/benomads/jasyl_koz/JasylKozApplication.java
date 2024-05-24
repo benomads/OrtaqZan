213e5c982a3bd6efc5f445f043a6ca965e0d9f02
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2024-05-24 21:52:26 +0500

    initial commit

---
+++ src/main/java/benomads/jasyl_koz/JasylKozApplication.java
@@ -0,0 +1,11 @@
+package benomads.jasyl_koz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasylKozApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JasylKozApplication.class, args);
+    }
+}
