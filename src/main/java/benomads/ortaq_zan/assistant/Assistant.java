0cf72b05df7ee2f442ffd5a6e00ce42e8e21f805
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2024-05-25 09:19:31 +0500

    refactored and improved AssistantConfig.java, added @AiService to Assistant.java

--- src/main/java/benomads/ortaq_zan/assistant/Assistant.java
+++ src/main/java/benomads/ortaq_zan/assistant/Assistant.java
@@ -4,9 +4,10 @@ import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.TokenStream;
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
 
 
-
+@AiService
 public interface Assistant {
 
     String SYSTEM_MESSAGE = """
--- src/main/java/benomads/ortaq_zan/assistant/AssistantConfig.java
+++ src/main/java/benomads/ortaq_zan/assistant/AssistantConfig.java
@@ -1,23 +1,19 @@
 package benomads.ortaq_zan.assistant;
 
-import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
 import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.TokenWindowChatMemory;
 import dev.langchain4j.model.Tokenizer;
-import dev.langchain4j.model.chat.StreamingChatLanguageModel;
 import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
-import dev.langchain4j.model.openai.OpenAiTokenizer;
-import dev.langchain4j.retriever.EmbeddingStoreRetriever;
-import dev.langchain4j.retriever.Retriever;
-import dev.langchain4j.service.AiServices;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import static dev.langchain4j.data.document.loader.FileSystemDocumentLoader.loadDocument;
-
-import org.springframework.beans.factory.annotation.Value;
+import static dev.langchain4j.data.document.splitter.DocumentSplitters.recursive;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,13 +22,6 @@ import org.springframework.core.io.ResourceLoader;
 @Configuration
 public class AssistantConfig {
 
-    @Bean
-    StreamingChatLanguageModel streamingChatLanguageModel(@Value("${OPEN_AI_API_KEY}") String apiKey) {
-        return OpenAiStreamingChatModel.builder()
-            .modelName("gpt-4o")
-            .apiKey(apiKey)
-            .build();
-    }
 
     @Bean
     EmbeddingModel embeddingModel() {
@@ -45,55 +34,39 @@ public class AssistantConfig {
     }
 
     @Bean
-    Retriever<TextSegment> retriever(EmbeddingModel embeddingModel,
-                                     EmbeddingStore<TextSegment> embeddingStore) {
-        return EmbeddingStoreRetriever.from(
-            embeddingStore,
-            embeddingModel,
-            1,
-            0.3
-        );
+    ContentRetriever retriever(EmbeddingModel embeddingModel,
+                               EmbeddingStore<TextSegment> embeddingStore) {
+        return EmbeddingStoreContentRetriever.builder()
+            .embeddingStore(embeddingStore)
+            .embeddingModel(embeddingModel)
+            .maxResults(1)
+            .minScore(0.6)
+            .build();
     }
 
     @Bean
-    CommandLineRunner docsToEmbedding(EmbeddingModel embeddingModel,
+    CommandLineRunner ingestDocsForLangChain(EmbeddingModel embeddingModel,
                                      EmbeddingStore<TextSegment> embeddingStore,
                                      Tokenizer tokenizer,
                                      ResourceLoader resourceLoader) {
         return args -> {
             var resource = resourceLoader.getResource("classpath:data/some-test-2.txt");
-            var loadDocument = loadDocument(resource.getFile().toPath());
-            var splitter = DocumentSplitters.recursive(100,
-                                                        0, tokenizer);
+            var termsOfUse = loadDocument(resource.getFile().toPath(), new TextDocumentParser());
             var ingestor = EmbeddingStoreIngestor.builder()
-                .embeddingStore(embeddingStore)
+                .documentSplitter(recursive(50, 0, tokenizer))
                 .embeddingModel(embeddingModel)
-                .documentSplitter(splitter)
+                .embeddingStore(embeddingStore)
                 .build();
-
-            ingestor.ingest(loadDocument);
+            ingestor.ingest(termsOfUse);
         };
     }
 
     @Bean
-    Tokenizer tokenizer() {
-        return new OpenAiTokenizer("gpt-4o");
+    ChatMemoryProvider chatMemoryProvider(Tokenizer tokenizer) {
+        // Tokenizer is provided by langchain4j-open-ai-spring-boot-starter
+        return chatId -> TokenWindowChatMemory.withMaxTokens(1000, tokenizer);
     }
 
-    @Bean
-    Assistant assistant(StreamingChatLanguageModel streamingChatLanguageModel,
-                        Tokenizer tokenizer,
-                        Retriever<TextSegment> retriever) {
-
-        return AiServices.builder(Assistant.class)
-            .streamingChatLanguageModel(streamingChatLanguageModel)
-            .chatMemoryProvider(memoryId -> TokenWindowChatMemory.builder()
-                .id(memoryId)
-                .maxTokens(1000, tokenizer)
-                .build())
-            .retriever(retriever)
-            .build();
-    }
 
 
 }
